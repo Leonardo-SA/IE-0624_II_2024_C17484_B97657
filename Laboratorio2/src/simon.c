46649ec2cc1ab16320cb46bbdcea51ae70bbcc93
Loresol1 <72949783+Loresol1@users.noreply.github.com>
2024-09-18 23:12:54 -0600

    Update final de simon.c

--- Laboratorio2/src/simon.c
+++ Laboratorio2/src/simon.c
@@ -26,23 +26,27 @@ int milliseconds = 0;           // Cuenta milisegundos
 
 // Funciones para controlar los LEDs
 void TurnOnGreen(void) {
-    PORTB = (1 << PB1);  // Enciende luz verde
+    PORTB = (0 << PB0) | (1 << PB1) | (0 << PB2) | (0 << PB3);  // Enciende luz verde
 }
 
 void TurnOnBlue(void) {
-    PORTB = (1 << PB3);  // Enciende luz azul
+    PORTB = (0 << PB0) | (0 << PB1) | (0 << PB2) | (1 << PB3);  // Enciende luz azul
 }
 
 void TurnOnRed(void) {
-    PORTB = (1 << PB2);  // Enciende luz roja
+    PORTB = (0 << PB0) | (0 << PB1) | (1 << PB2) | (0 << PB3);  // Enciende luz roja
 }
 
 void TurnOnYellow(void) {
-    PORTB = (1 << PB0);  // Enciende luz amarilla
+    PORTB = (1 << PB0) | (0 << PB1) | (0 << PB2) | (0 << PB3);  // Enciende luz amarilla
 }
 
 void TurnOffAll(void) {
-    PORTB = 0x00;  // Apaga todas las luces
+    PORTB = (0 << PB0) | (0 << PB1) | (0 << PB2) | (0 << PB3);  // Apaga todas las luces
+}
+
+void BlinkAll(void) {
+    PORTB ^= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3);  // Parpadeo de todas las luces
 }
 
 // Función para generar una nueva secuencia de colores
@@ -56,15 +60,22 @@ void GenerateSequence(void) {
 
 // Configuración de los puertos de luces y temporizador
 void SetupLights(void) {
-    DDRB = 0x0F;  // Configuración del puerto B
-    TCCR0B = 0x5; // Configuración del reloj del temporizador
+    GIMSK |= 0xD8;              // Habilita interrupciones para los pines de los puertos B y C
+    PCMSK2 |= 0b00000001;        // Interrupción para el pin PD0
+    PCMSK1 |= 0b00000001;        // Interrupción para el pin PA0
+    DDRB = 0x0F;                 // Configuración del puerto B
+    TCCR0B = 0x5;                // Configuración del reloj del temporizador
+    MCUCR |= (1 << ISC11);       // Configuración para flanco negativo en INT1
 }
 
 // Inicialización del temporizador
 void SetupTimer(void) {
     TCCR0A = 0x00;               // Modo de operación normal
+    TCCR0B = 0x00;
     TCCR0B |= (1 << CS00) | (1 << CS02);  // Prescaler para el temporizador
     sei();                        // Habilita interrupciones globales
+
+    TCNT0 = 0;                    // Inicializa el Timer0
     TIMSK |= (1 << TOIE0);        // Activa la interrupción por desbordamiento del Timer0
 }
 
@@ -72,19 +83,26 @@ void SetupTimer(void) {
 void FSMStatusChange(void) {
     switch (current_state) {
         case WAIT_TIME:
+            // Espera a que se presione un botón (color seleccionado)
             if (selected_color < 5) {
                 current_state = START;
+                timer_overflow_count = 0;  // Reinicia contadores
+                seconds = 0;
                 milliseconds = 0;
-                sequence_position = 0;
-                selected_color = 5;  // Desactiva el botón
             }
             break;
 
         case START:
-            if (milliseconds == 5) {
+            // Inicia la secuencia del juego
+            if (milliseconds == 4) {
                 current_state = GAME_SEQUENCE;
+                timer_overflow_count = 0;
+                seconds = 0;
                 milliseconds = 0;
                 sequence_position = 0;
+                sequence_length = 2;
+                selected_color = 5;  // Desactiva el botón
+                GenerateSequence();  // Genera nueva secuencia
             }
             break;
 
@@ -100,14 +118,19 @@ void FSMStatusChange(void) {
                 TurnOnYellow();
             }
 
+            // Verifica si ha pasado el tiempo para mostrar el siguiente color
             if (milliseconds == display_time) {
                 if (sequence_position >= sequence_length - 1) {
                     current_state = USER_SEQUENCE;
+                    timer_overflow_count = 0;
+                    seconds = 0;
                     milliseconds = 0;
                     TurnOffAll();  // Apaga todas las luces
                     sequence_position = 0;
                 } else {
                     sequence_position++;
+                    current_state = GAME_SEQUENCE;
+                    seconds = 0;
                     milliseconds = 0;
                     TurnOffAll();  // Apaga todas las luces
                 }
@@ -115,21 +138,41 @@ void FSMStatusChange(void) {
             break;
 
         case USER_SEQUENCE:
+            // Espera la secuencia del usuario
             if (selected_color == 5) {
                 current_state = USER_SEQUENCE;
+                milliseconds = 0;
             } else {
-                if (selected_color == color_sequence[sequence_position]) {
-                    selected_color = 5;
-
-                    if (sequence_position == sequence_length - 1) {
-                        current_state = END;
-                    } else {
-                        sequence_position++;
-                    }
-                } else {
-                    current_state = END;
+                if (selected_color == 0) {
+                    TurnOnGreen();
+                } else if (selected_color == 1) {
+                    TurnOnRed();
+                } else if (selected_color == 2) {
+                    TurnOnBlue();
+                } else if (selected_color == 3) {
+                    TurnOnYellow();
                 }
-                TurnOffAll();  // Apaga todas las luces
+
+                if (milliseconds == 2) {
+                    milliseconds = 0;
+                    TurnOffAll();  // Apaga todas las luces
+
+                    if (selected_color == color_sequence[sequence_position]) {
+                        selected_color = 5;
+
+                        if (sequence_position == sequence_length - 1) {
+                            current_state = LEVEL;
+                            seconds = 0;
+                            milliseconds = 0;
+                        } else {
+                            current_state = USER_SEQUENCE;
+                            sequence_position++;
+                        }
+                    } else {
+                        TurnOffAll();  // Apaga todas las luces en caso de error
+                        current_state = END;
+                    }
+                }
             }
             break;
 
@@ -146,7 +189,7 @@ void FSMStatusChange(void) {
             break;
 
         case END:
-            // Fin del juego
+            // Fin del juego: vuelve al estado de espera
             if (milliseconds == 6) {
                 current_state = WAIT_TIME;
                 selected_color = 5;
@@ -170,7 +213,24 @@ int main(void) {
 
 // Interrupciones
 ISR(TIMER0_OVF_vect) {  // Interrupción por desbordamiento del temporizador
-    milliseconds++;
+    if (timer_overflow_count % 12 == 0) {  // Medio segundo
+        if (current_state == START || current_state == END) {
+            BlinkAll();  // Parpadeo de todas las luces
+        }
+        ++milliseconds;  // Incrementa milisegundos
+    }
+
+    if (timer_overflow_count == 60) {  // Un segundo completo
+        timer_overflow_count = 0;
+        ++seconds;  // Incrementa el contador de segundos
+    } else {
+        timer_overflow_count++;
+    }
+
+    cycle_counter++;  // Incrementa el contador de ciclos
+    if (cycle_counter > 1000) {
+        cycle_counter = 0;
+    }
 }
 
 ISR(INT1_vect) {  // Interrupción - Amarillo
